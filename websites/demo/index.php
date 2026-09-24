3cc121a4c95d4d446ad38a20fea1010a7b982afb
paulo-politecnicllevant <pescanellas925@alumnes.politecnicllevant.cat>
2026-09-24 13:05:01 +0200

    EP08- Creo una función para filtrar los libros según el autor indicado

--- websites/demo/index.php
+++ websites/demo/index.php
@@ -10,23 +10,43 @@
     <?php
         $books = [
             [
-                'name' => "Do Androids Dream of Electric Sheep",
-                'author' => "Philip K. Dick",
-                'purchaseUrl' => "http://example.com"
+                'name' => 'Do Androids Dream of Electric Sheep',
+                'author' => 'Philip K. Dick',
+                'releaseYear' => '1968',
+                'purchaseUrl' => 'http://example.com'
             ],
             [
-                'name' => "Project Hail Mary",
-                'author' => "Andy Wair",
-                'purchaseUrl' => "http://example.com"
+                'name' => 'Project Hail Mary',
+                'author' => 'Andy Weir',
+                'releaseYear' => '2021',
+                'purchaseUrl' => 'http://example.com'
+            ],
+            [
+                'name' => 'The Martirian',
+                'author' => 'Andy Weir',
+                'releaseYear' => '2011',
+                'purchaseUrl' => 'http://example.com'
             ]
         ];
+
+        function filterByAuthor($books, $author){
+            $filteredBooks = [];
+
+            foreach ($books as $book){
+                if ($book['author'] === $author){
+                    $filteredBooks[] = $book;
+                }
+            }
+
+            return $filteredBooks;
+        }
     ?>
 
     <ul>
-        <?php foreach($books as $book) : ?>
+        <?php foreach(filterByAuthor($books, 'Philip K. Dick') as $book) : ?>
             <li>
                 <a href="<?= $book['purchaseUrl'] ?>">
-                    <?= $book['name'] ?>
+                    <?= $book['name'] ?> (<?= $book['releaseYear'] ?>) -By <?= $book['author'] ?>
                 </a>
             </li>
         <?php endforeach; ?>
