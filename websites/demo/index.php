89fda8e4d8de4471876af214b0cb2b9bfc0b2f61
paulo-politecnicllevant <pescanellas925@alumnes.politecnicllevant.cat>
2026-09-24 12:26:58 +0200

    EP05- Utilizo booleans y condicionales para indicar que informaión mostrar

--- websites/demo/index.php
+++ websites/demo/index.php
@@ -3,15 +3,33 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <style>
+        body{
+            display: grid;
+            place-items: center;
+            height: 100vh;
+            margin: 0;
+            font-family: sans-serif;
+        }
+    </style>
 </head>
 <body>
 
     <h1>
         <?php
-            $greeting = "Hello";
+            $name = "Dark Matter";
+            $read = true;
 
-            echo "$greeting Everybody!";
+            if($read) {
+                $message = "You have read $name";
+            }else{
+                $message = "You have NOT read $name";
+            }
         ?>
+
+        <h1>
+            <?= $message ?>
+        </h1>
     </h1>
 
 </body>
