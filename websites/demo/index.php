0c369bcc75f60fcfba3074bb127d8dd97dc338ce
paulo-politecnicllevant <pescanellas925@alumnes.politecnicllevant.cat>
2026-09-24 13:28:14 +0200

    EP09- Flexibilizo la función de filtrar para poder filtrar los libros en base a los parámetros que indique

--- websites/demo/index.php
+++ websites/demo/index.php
@@ -26,27 +26,19 @@
                 'author' => 'Andy Weir',
                 'releaseYear' => '2011',
                 'purchaseUrl' => 'http://example.com'
-            ]
+            ],
         ];
 
-        function filterByAuthor($books, $author){
-            $filteredBooks = [];
-
-            foreach ($books as $book){
-                if ($book['author'] === $author){
-                    $filteredBooks[] = $book;
-                }
-            }
-
-            return $filteredBooks;
-        }
+        $filteredBooks = array_filter($books, function($book){
+            return $book['author'] === 'Andy Weir';
+        })
     ?>
 
     <ul>
-        <?php foreach(filterByAuthor($books, 'Philip K. Dick') as $book) : ?>
+        <?php foreach($filteredBooks as $book) : ?>
             <li>
                 <a href="<?= $book['purchaseUrl'] ?>">
-                    <?= $book['name'] ?> (<?= $book['releaseYear'] ?>) -By <?= $book['author'] ?>
+                    <?= $book['name'] ?> (<?= $book['releaseYear'] ?>) - By <?= $book['author'] ?>
                 </a>
             </li>
         <?php endforeach; ?>
